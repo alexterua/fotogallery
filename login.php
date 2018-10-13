f27366cbf65e00ab046c562485fc9160f096be48
alexterua <alexter.od.ua@gmail.com>
2018-10-14 00:36:25 +0300

    password security

--- login.php
+++ login.php
@@ -5,12 +5,12 @@ require_once 'functions.php';
 
 session_start();
 
+$_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
 $_SESSION = $_POST;
 
 if (isset($_SESSION['login']) && isset($_SESSION['password'])) {
     $login = $_SESSION['login'];
     $password = password_verify($_SESSION['password'], PASSWORD_DEFAULT);
-
     header("Location: /");
 } else {
     include('login.view.php');
